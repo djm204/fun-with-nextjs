136c7aa9ce7ec43c4aaa7f1b0967c3105a84f3e1
David Mendez <desiratech@gmail.com>
2020-11-10 23:29:06 -0600

    add links

---
+++ example-nextjs-blog/pages/todo-items/item0.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const Item = () => {
+  return (
+    <>
+      <h1>Oi Do The F00kin Dishes Mate</h1>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </>
+  )
+}
+
+export default Item
