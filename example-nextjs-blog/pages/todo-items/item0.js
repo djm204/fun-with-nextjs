bc126993518419b52bfaca8fce505a9e49b60372
David Mendez <desiratech@gmail.com>
2021-07-25 19:47:45 -0500

    test

--- example-nextjs-blog/pages/todo-items/item0.js
+++ example-nextjs-blog/pages/todo-items/item0.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Item = () => {
   return (
     <>
-      <h1>Oi Do The F00kin Dishes Mate</h1>
+      <h1>Oi Do The F0ss0kin Dishes Mate</h1>
       <Link href="/">
         <a>Back to home</a>
       </Link>
